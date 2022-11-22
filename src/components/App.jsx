c753181fbeffc2eb86cbe3ea343b2fa2a7e502d3
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2022-11-22 17:13:08 +0200

    add styles and props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
-import Section from './Section';
-import ContactForm from "./ContactForm";
+import ContactForm from './ContactForm';
 import React, { Component } from 'react';
 import ContactList from './ContactList';
 import { nanoid } from 'nanoid';
 import Filter from './Filter';
 import Notiflix from 'notiflix';
-
+import { PhoneBook, PhonebookContainer, ContactsTitle } from './App.styled';
 
 class App extends Component {
   state = {
@@ -16,24 +15,29 @@ class App extends Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
- 
   };
- 
-  addContact = ({name,number}) => {
+
+  addContact = ({ name, number }) => {
     const contact = {
       id: nanoid(),
       name,
       number,
     };
 
-        const { contacts } = this.state;
+    const { contacts } = this.state;
 
     if (
       contacts.find(
-        contact => contact.name.toLowerCase() === name.toLowerCase(),
+        contact => contact.name.toLowerCase() === name.toLowerCase()
       )
     ) {
-      Notiflix.Notify.warning(`${name} is already in contacts.`);
+      Notiflix.Notify.warning(`${name} is already in contacts.`, {
+        width: '500px',
+        height: '40px',
+        backOverlay: true,
+        clickToClose: true,
+        closeButton: true,
+      });
     } else if (contacts.find(contact => contact.number === number)) {
       Notiflix.Notify.warning(`${number} is already in contacts.`);
     } else {
@@ -41,36 +45,53 @@ class App extends Component {
         contacts: [contact, ...contacts],
       }));
     }
-   
-  }
-  
-  deleteContact = (contactId) => {
-    this.setState(prevState => ({
-      contacts:prevState.contacts.filter(contact => contact.id !== contactId),
-    }))
-  }
+  };
 
-  changeFilter = (e) => {
-    this.setState({filter:e.currentTarget.value})
-  }
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
 
   render() {
-    const { filter,contacts } = this.state;
+    const { filter, contacts } = this.state;
     const normalizedFilter = this.state.filter.toLowerCase();
-    const filterContacts = this.state.contacts.filter(contact=>contact.name.toLowerCase().includes(normalizedFilter))
+    const filterContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
     return (
-      <div>
-        <Section text={'Phonebook'} />
+      <PhonebookContainer>
+        <PhoneBook>Phonebook</PhoneBook>
         <ContactForm onSubmit={this.addContact} />
-       <Filter value={filter} onChange={this.changeFilter} />
+        <Filter value={filter} onChange={this.changeFilter} />
 
-        <Section text={'Contacts'} />
-        {contacts.length > 0 ?
-          (<ContactList contacts={filterContacts} onDeleteContact={this.deleteContact} />) : (Notiflix.Notify.info("Your phonebook is empty. Please add contact."))
-        }
-
-      
-      </div>
+        <ContactsTitle>Contacts</ContactsTitle>
+        {contacts.length > 0 ? (
+          <ContactList
+            contacts={filterContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        ) : (
+          Notiflix.Notify.info('Your phonebook is empty. Please add contact.', {
+            position: 'center-bottom',
+            backOverlay: true,
+            clickToClose: true,
+            closeButton: true,
+            info: {
+              background: '#8f9a9b',
+              textColor: '#0c0b0b',
+              childClassName: 'notiflix-notify-info',
+              notiflixIconColor: '#f7f4f4',
+              fontAwesomeClassName: 'fas fa-info-circle',
+              backOverlayColor: 'rgba(38,192,211,0.2)',
+            },
+          })
+        )}
+      </PhonebookContainer>
     );
   }
 }
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
+import { Form, Label, Input, ButtonAdd } from './ContactForm.styled';
 
 class ContactForm extends Component {
   state = {
@@ -29,32 +30,37 @@ class ContactForm extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor={this.nameInputId}>Name</label>
-        <input
-          type="text"
-          name="name"
-          id={this.nameInputId}
-          value={this.state.name}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          onChange={this.handleChange}
-        />
+      <Form onSubmit={this.handleSubmit}>
+        <Label htmlFor={this.nameInputId}>
+          Name
+          <Input
+            type="text"
+            name="name"
+            id={this.nameInputId}
+            value={this.state.name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChange}
+          />
+        </Label>
 
-        <label htmlFor={this.numberInputId}>Number</label>
-        <input
-          type="tel"
-          name="number"
-          id={this.numberInputId}
-          value={this.state.number}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          onChange={this.handleChange}
-        />
-        <button type="submit">Add contact</button>
-      </form>
+        <Label htmlFor={this.numberInputId}>
+          Number
+          <Input
+            type="tel"
+            name="number"
+            id={this.numberInputId}
+            value={this.state.number}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            onChange={this.handleChange}
+          />
+        </Label>
+
+        <ButtonAdd type="submit">Add contact</ButtonAdd>
+      </Form>
     );
   }
 }
